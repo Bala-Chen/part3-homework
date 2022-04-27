77ab7051e6ced19ab7464d39c5b9f86792fc0bab
bala-chen <84770662+Bala-Chen@users.noreply.github.com>
2022-04-27 11:43:59 +0800

    Change index route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('view engine','ejs');
 app.use('/public',express.static('public'));
 
-app.get('/',(req,res)=>{
+app.get('/homework',(req,res)=>{
     res.render('index');
 })
 
